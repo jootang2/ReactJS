0a4403dbee49d8750f74a9fbc97bda25778cb431
joohwan <joohwan9607@mobility42.io>
2024-01-01 02:08:26 +0900

    event - preventDefault

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
-const loginInput = document.querySelector('#login-form input')
-const loginButton = document.querySelector('#login-form button')
+// 버튼을 누를 때 마다 submit 되면서 페이지가 새로고침 됨
+// => preventDefault함수를 통해서 브라우저의 기본 동작을 막아주면 해결
 
-function onLoginBtnClick() {
-    console.log(loginInput.value)
+const loginForm = document.querySelector('#login-form')
+const loginInput = document.querySelector('#login-form input')
+
+function onLoginSubmit(info) {
+    info.preventDefault();
+    const username = loginInput.value;
+    console.log(username)
 }
 
-loginButton.addEventListener('click', onLoginBtnClick)
+loginForm.addEventListener('submit', onLoginSubmit);
+
+
