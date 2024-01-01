0bbfea05f44d287b1318882664c9017890efde23
joohwan <joohwan9607@mobility42.io>
2024-01-02 00:04:04 +0900

    use localStorage

--- index.js
+++ index.js
@@ -3,13 +3,32 @@
 
 const loginForm = document.querySelector('#login-form')
 const loginInput = document.querySelector('#login-form input')
+const greeting = document.querySelector('#greeting');
+
+const HIDDEN_CLASSNAME = 'hidden';
+const USERNAME_KEY = 'username';
+const savedUsername = localStorage.getItem(USERNAME_KEY);
 
 function onLoginSubmit(info) {
     info.preventDefault();
     const username = loginInput.value;
-    console.log(username)
+    loginForm.classList.add(HIDDEN_CLASSNAME)
+    localStorage.setItem(USERNAME_KEY, username);
+    greeting.innerText = `Hello ${username}`
+    greeting.classList.remove(HIDDEN_CLASSNAME)
 }
 
-loginForm.addEventListener('submit', onLoginSubmit);
+function paintGreetings(username) {
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+  }
 
+
+
+if(savedUsername === null){
+    loginForm.classList.remove(HIDDEN_CLASSNAME)
+    loginForm.addEventListener('submit', onLoginSubmit);
+} else {
+    paintGreetings(savedUsername);
+}
 
