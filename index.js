a02ac2f5efd05f3968624848ff862b4755de692a
joohwan <joohwan9607@mobility42.io>
2024-01-03 01:18:11 +0900

    vanilaJs -> reactJs 2

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-const button = document.querySelector('#btn');
-let counter = 0;
-function handleBtnClick() {
-    const count = document.querySelector('#counter');
-    counter ++;
-    count.innerHTML = `Total Click : ${counter}`
+const root = document.querySelector('#root')
 
-}
-button.addEventListener('click', handleBtnClick);
+const span = React.createElement('h3', {
+    onMouseEnter: () => console.log('mouse enter')
+}, 'TEST')
+
+const button = React.createElement('button', {
+    onClick: () => {console.log('clicked')}
+}, 'Click me')
+
+const container = React.createElement('div', null, [span, button])
+
+ReactDOM.render(container, root);
