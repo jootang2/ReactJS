7160a7ad2eb8ad9f11799a6f5a144903b1026c76
joohwan <joohwan9607@mobility42.io>
2024-01-14 01:23:18 +0900

    useEffect()

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,11 +1,18 @@
 
+import { useEffect, useState } from 'react';
 import styles from './App.module.css'
 import Button from './Button';
 function App() {
+  const [counter, setCounter] = useState(0);
+  const onClick = (() => {
+    setCounter((prev) => prev + 1)
+  })
+  useEffect(() => {console.log('this is useEffect')}, [])
   return (
     <div>
-      <button className={styles.btn}>Hello world</button>
-      <Button text={'start react'}/>
+      <div>{counter}</div>
+      <button onClick={onClick} className={styles.btn}>Hello world</button>
+      <Button text={'counter'}/>
     </div>
   );
 }
